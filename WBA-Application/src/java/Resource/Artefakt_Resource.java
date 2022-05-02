6c40c3bc0cf12c131343024af8b25e8e10742e8f
Nico <82504489+d3micas@users.noreply.github.com>
2022-05-02 21:59:17 +0200

    added Get Method

--- WBA-Application/src/java/Resource/Artefakt_Resource.java
+++ WBA-Application/src/java/Resource/Artefakt_Resource.java
@@ -4,10 +4,30 @@
  */
 package Resource;
 
+import Classes.Artefakt;
+import java.net.URI;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author Nico
  */
 public class Artefakt_Resource {
     
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(ProjektAdapter pa){
+        Artefakt artefakt = new Artefakt();
+        URI location = URI.create("/artefakt?title=" + artefakt.getTitle());
+        Response.ResponseBuilder rb = Response.created(location); 
+        
+        URI delLocLink = URI.create("/artefakt/delete?title=" + artefakt.getTitle());
+        rb.link(delLocLink, "delete");
+        return rb.build();
+    }
 }
