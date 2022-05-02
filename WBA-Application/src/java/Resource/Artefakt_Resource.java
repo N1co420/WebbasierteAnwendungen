e32fc0e07766daf891e861a2c7c27f28388de2d1
Nico <82504489+d3micas@users.noreply.github.com>
2022-05-02 22:00:50 +0200

    added Get Method (prev was Post)

--- WBA-Application/src/java/Resource/Artefakt_Resource.java
+++ WBA-Application/src/java/Resource/Artefakt_Resource.java
@@ -7,8 +7,10 @@ package Resource;
 import Classes.Artefakt;
 import java.net.URI;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -28,6 +30,16 @@ public class Artefakt_Resource {
         
         URI delLocLink = URI.create("/artefakt/delete?title=" + artefakt.getTitle());
         rb.link(delLocLink, "delete");
+        return rb.build();
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@QueryParam("id") Long id){
+        Artefakt artefakt = new Artefakt();
+        artefakt.setTitle("Artefakttitel");
+        Response.ResponseBuilder rb = Response.ok(artefakt);
+        
         return rb.build();
     }
 }
